06e4201e3d7b63e1bfc888d4d96c7c111625da46
sk1nny <yagoluiz28@gmail.com>
2022-05-26 19:40:23 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Layout, { GradientBackground } from '../components/Layout';
 import ArrowIcon from '../components/ArrowIcon';
 import { getGlobalData } from '../utils/global-data';
 import SEO from '../components/SEO';
-import Script from 'next/script'
+import Home from 'next/script'
 
 export default function Index({ posts, globalData }) {
   return (
